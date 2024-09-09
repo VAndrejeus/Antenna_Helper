96d211b71ed86c57bf5831ceb0672bd875d09089
andre <andrejeusv@gmail.com>
2024-09-09 15:46:57 -0400

    Initial commit

---
+++ .venv/main.py
@@ -0,0 +1,15 @@
+# Main program file that creates the windows and all of the elements
+from tkinter import *
+
+# Define the main window
+window = Tk()
+window.title("Antenna Helper")
+window.minsize(width=300,height=300)
+# Main program elements
+
+
+window.mainloop()
+
+
+
+
