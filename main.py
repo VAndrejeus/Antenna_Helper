4481f33d6f6ac992afaa8ca0ca3096740ca51dc9
andre <andrejeusv@gmail.com>
2024-09-18 19:37:51 -0400

    Removed lone Yagi label and entry. -Added main frame -Added Notebook to hold antenna tabs -Added Yagi, Moxon, Dipole tabs -Added Yagi, Moxon, Dipole Freq Entry fields

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Main program file that creates the windows and all of the elements
 from tkinter import *
+from tkinter import ttk
 
 # ---------------------YAGI formula-------------------
 '''         Lengths: 
@@ -34,8 +35,29 @@ window.title("Antenna Helper")
 window.minsize(width=300,height=300)
 # Main program elements
 
-label_yagi = Label(text="Yagi Antenna")
-label_yagi.grid(column=0, row=1)
+# Frame that will hold most of the tabs for different antennas
+tabs_base = Frame(window)
+tabs_base.grid(column=0, row=0)
+
+# Notebook that will have tabs for different antenna types
+antenna_tabs = ttk.Notebook(tabs_base)
+antenna_tabs.grid(column=0, row=0)
+# Yagi tab
+yagi_tab = ttk.Frame(antenna_tabs)
+antenna_tabs.add(yagi_tab, text="Yagi Antenna")
+freq_field_yagi = ttk.Entry(yagi_tab)# Entry field for the frequency
+freq_field_yagi.grid(column=0, row=0)
+# Moxon tab
+moxon_tab = ttk.Frame(antenna_tabs)
+antenna_tabs.add(moxon_tab, text="Moxon Antenna")
+freq_field_moxon = ttk.Entry(moxon_tab)# Entry field for the frequency
+freq_field_moxon.grid(column=0, row=0)
+# Dipole tab
+dipole_tab = ttk.Frame(antenna_tabs)
+antenna_tabs.add(dipole_tab, text="Dipole Antenna")
+freq_field_dipole = ttk.Entry(dipole_tab)# Entry field for the frequency
+freq_field_dipole.grid(column=0, row=0)
+
 
 window.mainloop()
 
