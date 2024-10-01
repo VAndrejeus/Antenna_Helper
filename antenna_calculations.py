a9b1823ebcea84b445cf98b2fdc949cdc6e3c823
andre <andrejeusv@gmail.com>
2024-10-01 14:34:49 -0400

    Antenna_calculations.py: -Updated calculate_yagi function with measurement elements, limited to 3 points float
    main.py
    -Updated Yagi image
    -Added antenna measurements to the Yagi picture as canvas.create_text
    -Updated calc_button_yagi with all measuring elements
    -Added fonts constant variable

--- antenna_calculations.py
+++ antenna_calculations.py
@@ -9,27 +9,44 @@ def calculate_wavelength(freq):
 
 # Calculate Wavelengths
 def calculate_lengths(wavelength):
-    reflector = .495 * wavelength
-    dipole_radiator = .473 * wavelength
-    director1 = .440 * wavelength
-    director2 = .435 * wavelength
-    director3 = .430 * wavelength
+    reflector = 0.495 * wavelength
+    dipole_radiator = 0.473 * wavelength
+    director1 = 0.440 * wavelength
+    director2 = 0.435 * wavelength
+    director3 = 0.430 * wavelength
     return reflector, dipole_radiator, director1, director2, director3
 
 
 # Calculate Separation between elements
 def calculate_separation(wavelength):
-    re_dr = .125 * wavelength
-    dr_d1 = .125 * wavelength
-    d1_d2 = .250 * wavelength
-    d2_d3 = .250 * wavelength
+    re_dr = 0.125 * wavelength
+    dr_d1 = 0.125 * wavelength
+    d1_d2 = 0.250 * wavelength
+    d2_d3 = 0.250 * wavelength
     return re_dr, dr_d1, d1_d2, d2_d3
 
 
 # Main calculate Yagi function
-def calculate_yagi(freq, label_field):
-    result = calculate_wavelength(freq)
-    label_field(text=f"Full WL = {result[0]}")  # Display only Full Wl for now( 0 index o a tupple
+def calculate_yagi(freq, wl_label_field, canvas, l_re, l_dr, l_d1, l_d2, l_d3, d_dr_re, d_d1_dr, d_d1_d2, d_d2_d3):
+    wave_length = calculate_wavelength(freq)[0]
+    wl_label_field(text=f"Full WL = {wave_length}")  # Display only Full Wl for now( 0 index o a tupple
+
+    #Lengths of elements
+    lengths = calculate_lengths(wave_length)
+    canvas.itemconfig(l_re, text=f"{lengths[0]:.3f} ft")
+    canvas.itemconfig(l_dr, text=f"{lengths[1]:.3f} ft")
+    canvas.itemconfig(l_d1, text=f"{lengths[2]:.3f} ft")
+    canvas.itemconfig(l_d2, text=f"{lengths[3]:.3f} ft")
+    canvas.itemconfig(l_d3, text=f"{lengths[4]:.3f} ft")
+
+    #Distance between elements
+    distances = calculate_separation(wave_length)
+    canvas.itemconfig(d_dr_re, text=f"{distances[0]:.3f} ft")
+    canvas.itemconfig(d_d1_dr, text=f"{distances[1]:.3f} ft")
+    canvas.itemconfig(d_d1_d2, text=f"{distances[2]:.3f} ft")
+    canvas.itemconfig(d_d2_d3, text=f"{distances[3]:.3f} ft")
+
+
 
 
 # ---------------------Moxon formula-------------------
--- main.py
+++ main.py
@@ -7,8 +7,12 @@ import antenna_calculations as ac
 window = Tk()
 window.title("Antenna Helper")
 window.minsize(width=300, height=300)
-# Main program elements
 
+# Main variables
+MEASUREMENT_FONT = ("Times New Roman", 12, "bold") # Dimensions font constant variable
+
+
+# Main program elements
 # Frame that will hold most of the tabs for different antennas
 tabs_base = Frame(window)
 tabs_base.grid(column=0, row=0)
@@ -26,17 +30,39 @@ freq_field_yagi = ttk.Entry(yagi_tab, )  # Entry field for the frequency
 freq_field_yagi.grid(column=0, row=0)
 
 
-yagi_wl_label = ttk.Label(yagi_tab, text="Wavelength") #Yagi Wavelength label
+yagi_wl_label = ttk.Label(yagi_tab, text="Wavelength", foreground="purple", font=MEASUREMENT_FONT) #Yagi Wavelength label
 yagi_wl_label.grid(column=1, row=0)
 
 calc_button_yagi = ttk.Button(yagi_tab, text="Calculate",
                               command=lambda: ac.calculate_yagi(freq_field_yagi.get(),
-                                                                yagi_wl_label.config))  # Create Yagi calculate button
+                                                                yagi_wl_label.config,
+                                                                canvas,
+                                                                length_reflector,
+                                                                length_driven,
+                                                                length_director1,
+                                                                length_director2,
+                                                                length_director3,
+                                                                d3_d2_distance,
+                                                                d2_d1_distance,
+                                                                d1_dr_distance,
+                                                                dr_re_distance
+                                                                ))
 calc_button_yagi.grid(column=0, row=1)
 # Yagi Image
 canvas = Canvas(yagi_tab, width=450, height=180)
 yagi_image = PhotoImage(file="Yagi_image.png")
 image = canvas.create_image(225, 90, image=yagi_image)
+# Element lengths
+length_reflector = canvas.create_text(420,100, text="0", fill="green", font=MEASUREMENT_FONT)
+length_driven = canvas.create_text(330, 20, text="0", fill="green", font=MEASUREMENT_FONT)
+length_director1 = canvas.create_text(245, 20, text="0", fill="green", font=MEASUREMENT_FONT)
+length_director2 = canvas.create_text(165, 20, text="0", fill="green", font=MEASUREMENT_FONT)
+length_director3 = canvas.create_text(80, 20, text="0", fill="green", font=MEASUREMENT_FONT)
+# Distances between elements
+d3_d2_distance = canvas.create_text(120,70, text="0", fill="green", font=MEASUREMENT_FONT)
+d2_d1_distance = canvas.create_text(205,70, text="0", fill="green", font=MEASUREMENT_FONT)
+d1_dr_distance = canvas.create_text(290, 70, text="0", fill="green", font=MEASUREMENT_FONT)
+dr_re_distance = canvas.create_text(355, 70, text="0", fill="green", font=MEASUREMENT_FONT)
 canvas.grid(column=0, row=2, columnspan=2)
 
 
@@ -48,7 +74,7 @@ freq_field_moxon = ttk.Entry(moxon_tab)  # Entry field for the frequency
 freq_field_moxon.grid(column=0, row=0)
 
 calc_button_moxon = ttk.Button(moxon_tab, text="Calculate",
-                               command=ac.calculate_moxon)  # Create Moxon calculate button
+                               command=ac.calculate_moxon())  # Create Moxon calculate button
 calc_button_moxon.grid(column=0, row=1)
 
 # Dipole tab
@@ -59,7 +85,7 @@ freq_field_dipole = ttk.Entry(dipole_tab)  # Entry field for the frequency
 freq_field_dipole.grid(column=0, row=0)
 
 calc_button_dipole = ttk.Button(dipole_tab, text="Calculate",
-                                command=ac.calculate_dipole)  #Create Dipole calculate button
+                                command=ac.calculate_dipole())  #Create Dipole calculate button
 calc_button_dipole.grid(column=0, row=1)
 
 window.mainloop()
